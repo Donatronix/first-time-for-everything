f3b8493cb718528f5e2a384e57666bcb72af9c87
Donald Blessing <donaldblessing9@gmail.com>
2022-11-21 10:52:26 +0100

    Added model and migration with cache trait

---
+++ app/Models/Profile.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spiritix\LadaCache\Database\LadaCacheTrait;
+
+class Profile extends Model
+{
+    use HasFactory;
+    use LadaCacheTrait;
+
+    protected $fillable = [
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
