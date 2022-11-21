78808159c0f5e72e665d04f649d1d4025c10e6d1
Donald Blessing <donaldblessing9@gmail.com>
2022-11-21 10:51:55 +0100

    Added cache trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,18 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Address;
+use App\Models\Profile;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spiritix\LadaCache\Database\LadaCacheTrait;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    use LadaCacheTrait;
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +45,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function address()
+    {
+        return $this->hasOne(Address::class, 'user_id');
+    }
+
+
+    public function profile()
+    {
+        return $this->hasOne(Profile::class, 'user_id');
+    }
 }
