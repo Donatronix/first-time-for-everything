19c5f9c39ccb66d925d603017b0e34c3c5180013
Donald Blessing <donaldblessing9@gmail.com>
2022-11-21 10:51:43 +0100

    Created model and migration and added cache trait

---
+++ app/Models/Address.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spiritix\LadaCache\Database\LadaCacheTrait;
+
+class Address extends Model
+{
+    use HasFactory;
+    use LadaCacheTrait;
+
+    protected $fillable = [
+        'street',
+        'town',
+        'state',
+        'country',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
